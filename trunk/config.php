3b212101894b10f8addd7ab15eda1b4bbf38e55a
captainsun <captainsun@b89c15bf-0d4d-5b37-a196-eb4625c90646>
2010-03-24 16:19:53 +0000

    Full project complete.

---
+++ trunk/config.php
@@ -0,0 +1,23 @@
+<?php
+//http://code.google.com/p/facebook-twitter-linkedin-status-update/wiki/introduction
+
+$config['app_title']            =   'Facebook, Twitter and Linkedin Status Update';
+$config['base_url']             =   ''; // http://thinkdiff.net/demo/fblinkedtwit
+$config['callback_url']         =   ''; //http://thinkdiff.net/demo/fblinkedtwit/index.php
+
+//facebook configuration
+$config['fb_api']               =   'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+$config['fb_secret']            =   'yyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy';
+$config['facebook_library_path']=   'facebook/facebook.php';
+
+//linkedin configuration
+$config['linkedin_access']      =   'xxx your linkedin application access key';
+$config['linkedin_secret']      =   'yyy your linkedin application secret key';
+$config['linkedin_library_path']=   'linkedin_twitter/linkedinoAuth.php';
+
+//twitter configuration
+$config['twitter_consumer']     =   'xxxxxx your app consumer key';
+$config['twitter_secret']       =   'your app secret key';
+$config['twitter_library_path'] =   'linkedin_twitter/twitterOAuth.php';
+
+?>
